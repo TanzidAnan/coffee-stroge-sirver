db8680db5c458ec22e850266a33565c25ef965f9
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-27 11:15:03 +0600

    commit

--- simple-crud-sirver/index.js
+++ simple-crud-sirver/index.js
@@ -27,6 +27,10 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("usersdb").collection("movies");
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
